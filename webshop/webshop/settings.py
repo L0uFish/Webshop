bb31ffe42f08f1d90eb25e9ef337737d6ec39349
L0uFish <wouterslouie@gmail.com>
2024-08-14 01:13:56 +0200

    testing

--- webshop/webshop/settings.py
+++ webshop/webshop/settings.py
@@ -15,7 +15,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'webshop.shop',
+    'webshop.webshop.shop',
 ]
 
 MIDDLEWARE = [
